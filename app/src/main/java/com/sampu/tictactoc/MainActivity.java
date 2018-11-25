21f151616963166f4f664d6c667c3e1844a5a8bd
Anupam Roy <anupam.roy.125@gmail.com>
2018-11-25 19:13:12 +0530

    First Commit

---
+++ app/src/main/java/com/sampu/tictactoc/MainActivity.java
@@ -0,0 +1,35 @@
+package com.sampu.tictactoc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    public void onplay(View V){
+    	Intent i = new Intent(this, GameActivity.class);
+    	startActivity(i);
+    }
+    public void exit(View V){
+    	finish();
+    }
+    
+}
