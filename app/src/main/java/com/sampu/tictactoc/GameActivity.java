bbaf544c1b53395d9556609d5ed2c7107a38ea6b
Anupam Roy <anupam.roy.125@gmail.com>
2018-12-09 14:53:44 +0530

    Added Sounds & Improved Layout

--- app/src/main/java/com/sampu/tictactoc/GameActivity.java
+++ app/src/main/java/com/sampu/tictactoc/GameActivity.java
@@ -1,5 +1,6 @@
 package com.sampu.tictactoc;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import android.app.Activity;
@@ -9,6 +10,7 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Gravity;
@@ -24,7 +26,7 @@ import android.widget.TextView;
 public class GameActivity extends Activity implements OnClickListener {
 	
 	private static Button[][] b=new Button[3][3];
-	private static ArrayList<Button> ab=new ArrayList<Button>();
+	private static ArrayList<Button> ab= new ArrayList<>();
 	private boolean win=false,block=false;
 	private boolean user,hard=true,com;
 	private int u,c;
@@ -33,62 +35,47 @@ public class GameActivity extends Activity implements OnClickListener {
 	private Runnable game,U;
 	private SharedPreferences sp;
 	private Dialog d;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(0,LinearLayout.LayoutParams.WRAP_CONTENT,1);
-		LinearLayout box=new LinearLayout(this);
-		box.setOrientation(LinearLayout.VERTICAL);
-		//box.setBackgroundColor(Color.GREEN);
-		TextView title=new TextView(this);
-		title.setTextSize(30);
-		title.setGravity(Gravity.CENTER);
-		title.setTextColor(Color.CYAN);
-		title.setText("Tic     Tac     Toc");
-		title.setHeight(50);
-		box.addView(title);
-		//box.setBackgroundResource(R.drawable.tictotac);
-		
+		setContentView(R.layout.activity_game);
+
+        b[0][0]=findViewById(R.id.one);
+		b[0][1]=findViewById(R.id.two);
+		b[0][2]=findViewById(R.id.three);
+		b[1][0]=findViewById(R.id.four);
+		b[1][1]=findViewById(R.id.five);
+		b[1][2]=findViewById(R.id.six);
+		b[2][0]=findViewById(R.id.seven);
+		b[2][1]=findViewById(R.id.eight);
+		b[2][2]=findViewById(R.id.nine);
+
 		 for (int i=0;i<3;i++) {
-	        	LinearLayout row = new LinearLayout(this);
-	        	row.setOrientation(LinearLayout.HORIZONTAL);
 	        	for(int j=0;j<3;j++) {
-	        		b[i][j]=new Button(this);
-	        		b[i][j].setLayoutParams(lp);
-	        		//b[i][j].setHeight(100);
-	        		//b[i][j].setAlpha((float) 0.9);
 	        		b[i][j].setOnClickListener(this);
 	        		b[i][j].setTypeface(Typeface.DEFAULT_BOLD);
 	        		b[i][j].setTextSize(70);
-	        		//b[i][j].setTextColor(Color.BLUE);
-	        		row.addView(b[i][j]);
 	        	}
-	        	box.addView(row);
 	        }
 		 
 		sp=getPreferences(MODE_PRIVATE);
 		u=sp.getInt("user", 0);
 		c=sp.getInt("com",0);
 		
-		TextView score=new TextView(this);
+		TextView score=findViewById(R.id.score);
 		score.setTextColor(Color.BLUE);
 		score.setTextSize(20);
 		score.setGravity(Gravity.CENTER);
 		score.setTypeface(Typeface.SANS_SERIF);
 		score.setText("You: "+u+"		Computer: "+c);
-		box.addView(score);
-		 
-		setContentView(box);//R.layout.activity_game);
 	}
-	
-	
-	
+
 
 	@Override
 	protected void onStart() {
-		// TODO Auto-generated method stub
+		
 		super.onStart();
 		
 		ab.clear();
@@ -107,27 +94,24 @@ public class GameActivity extends Activity implements OnClickListener {
 			
 			@Override
 			public void onClick(DialogInterface arg0, int arg1) {
-				// TODO Auto-generated method stub
+				
 				user=true;com=false;
 			}
 		}).setNegativeButton("Computer", new DialogInterface.OnClickListener() {
 			
 			@Override
 			public void onClick(DialogInterface arg0, int arg1) {
-				// TODO Auto-generated method stub
+				
 				user=false;com=true;
 			}
 		})
 		.setCancelable(false).show();
-		
-		//user=false;com=true;//user=true;com=false;
-		
-		
+
 		 U=new Runnable() {
 			
 			@Override
 			public void run() {
-				// TODO Auto-generated method stub
+				
 				System.out.println("USER MODE user="+user+"Com="+com);
 				if(user)
 					loop.postDelayed(this, 1000);
@@ -143,7 +127,7 @@ public class GameActivity extends Activity implements OnClickListener {
 			
 			@Override
 			public void run() {
-				// TODO Auto-generated method stub
+				
 				
 					user_turn();
 					
@@ -173,7 +157,7 @@ public class GameActivity extends Activity implements OnClickListener {
 								
 							}
 						});
-						
+						MediaPlayer.create(GameActivity.this,R.raw.user_win).start();
 						d.show();
 						
 						return;
@@ -199,19 +183,19 @@ public class GameActivity extends Activity implements OnClickListener {
 								finish();	
 							}
 						});
+						MediaPlayer.create(GameActivity.this,R.raw.draw_game).start();
 						d.show();
 						
 						return;
 					}
-				
-					try {
-						Thread.sleep(0);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				
-					if(com)
+
+				try {
+					Thread.sleep(400);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+				if(com)
 						com_turn();
 					
 					if(draw()){
@@ -234,6 +218,7 @@ public class GameActivity extends Activity implements OnClickListener {
 								finish();	
 							}
 						});
+						MediaPlayer.create(GameActivity.this,R.raw.draw_game).start();
 						d.show();
 						
 						return;
@@ -242,12 +227,10 @@ public class GameActivity extends Activity implements OnClickListener {
 			}
 
 			private void com_turn() {
-				// TODO Auto-generated method stub
-				//Toast.makeText(GameActivity.this, "Computer Turn com="+com, Toast.LENGTH_SHORT).show();
-				//Toast.makeText(GameActivity.this, "Com_turn.  block="+block+"user="+user, Toast.LENGTH_SHORT).show();
-				
+				//Com_Sound_player.start();
+				MediaPlayer.create(GameActivity.this,R.raw.com_turn).start();
+
 				if(com){
-					
 					wintry(b[0][0],b[0][1],b[0][2]);
 					wintry(b[1][0],b[1][1],b[1][2]);
 					wintry(b[2][0],b[2][1],b[2][2]);
@@ -272,6 +255,7 @@ public class GameActivity extends Activity implements OnClickListener {
 							Button ff=ab.get(lok);
 							ff.setText("O");
 							ab.remove(lok);
+							//User_Sound_player.stop();
 							//com=false;
 						}
 					}
@@ -286,7 +270,7 @@ public class GameActivity extends Activity implements OnClickListener {
 			private void user_turn() {
 				
 				System.out.println("User Turn");
-				// TODO Auto-generated method stub
+				
 
 				loop.post(U);
 				
@@ -309,9 +293,10 @@ public class GameActivity extends Activity implements OnClickListener {
 
 	@Override
 	public void onClick(View V) {
-		// TODO Auto-generated method stub
+		//User_Sound_player.start();
 		Button bb=(Button)V;
 		if(bb.getText().equals("")&&user){
+			MediaPlayer.create(GameActivity.this,R.raw.user_turn).start();
 			bb.setTextColor(Color.RED);
 			bb.setText("X");
 			//bb.setTextColor(Color.BLUE);
@@ -378,9 +363,9 @@ public class GameActivity extends Activity implements OnClickListener {
 				e.commit();
 				d.dismiss();
 				finish();
-						
 					}
-				});		
+				});
+				MediaPlayer.create(GameActivity.this,R.raw.com_win).start();
 				d.show();
 				
 				win=true;
@@ -394,10 +379,10 @@ public class GameActivity extends Activity implements OnClickListener {
 				d.setContentView(R.layout.my_dialog);
 				d.setCancelable(false);
 
-				ImageView logo=(ImageView) d.findViewById(R.id.imageView1);
+				ImageView logo=d.findViewById(R.id.imageView1);
 				logo.setImageResource(R.drawable.l);
 
-				Button b=(Button)d.findViewById(R.id.bb2);
+				Button b=d.findViewById(R.id.bb2);
 				b.setOnClickListener(new OnClickListener() {
 					
 					@Override
@@ -411,7 +396,8 @@ public class GameActivity extends Activity implements OnClickListener {
 				finish();
 						
 					}
-				});		
+				});
+				MediaPlayer.create(GameActivity.this,R.raw.com_win).start();
 				d.show();
 				
 				win=true;
@@ -486,9 +472,7 @@ public class GameActivity extends Activity implements OnClickListener {
 	
 	
 	 void blocktry(Button b1,Button b2,Button b3) {
-		 
-		// Toast.makeText(this, "Blocktry: ["+b1.getText()+"]  ["+b2.getText()+"]  ["+b3.getText()+"]", Toast.LENGTH_SHORT).show();
-		
+
 		if(!block){
 			if(b1.getText().equals("X")&&b2.getText().equals("X")&&b3.getText().equals("")){
 				b3.setText("O");
@@ -519,7 +503,7 @@ public class GameActivity extends Activity implements OnClickListener {
 
 	@Override
 	public void onBackPressed() {
-		// TODO Auto-generated method stub
+		
 		super.onBackPressed();
 		loop.removeCallbacks(U);
 		loop.removeCallbacks(game);
