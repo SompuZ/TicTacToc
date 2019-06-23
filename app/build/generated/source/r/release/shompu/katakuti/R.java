1fa6b7fbbbbf2f416c4430034aa7bdb670e09516
Anupam Roy <anupam.roy.125@gmail.com>
2019-06-23 21:25:23 +0530

    minor changes in drawable xml through inkscape

--- app/build/generated/source/r/release/shompu/katakuti/R.java
+++ app/build/generated/source/r/release/shompu/katakuti/R.java
@@ -16,8 +16,9 @@ public final class R {
     public static final int ic_drawgame=0x7f020000;
     public static final int ic_launcher=0x7f020001;
     public static final int ic_loser=0x7f020002;
-    public static final int ic_tictoctac=0x7f020003;
-    public static final int ic_winner=0x7f020004;
+    public static final int ic_tictactocboard=0x7f020003;
+    public static final int ic_tictoctac=0x7f020004;
+    public static final int ic_winner=0x7f020005;
   }
   public static final class id {
     public static final int action_settings=0x7f030000;
